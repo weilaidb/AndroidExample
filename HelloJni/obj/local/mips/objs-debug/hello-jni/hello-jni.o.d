eb510a6b744e90969d43a216ef34bdb71e62d826
Your Name <you@example.com>
2016-05-08 19:08:12 +0800

    hello,jni

---
+++ HelloJni/obj/local/mips/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,19 @@
+obj/local/mips/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ D:/Android/android-ndk-r10d/platforms/android-19/arch-mips/usr/include/string.h \
+ D:/Android/android-ndk-r10d/platforms/android-19/arch-mips/usr/include/sys/cdefs.h \
+ D:/Android/android-ndk-r10d/platforms/android-19/arch-mips/usr/include/sys/cdefs_elf.h \
+ D:/Android/android-ndk-r10d/platforms/android-19/arch-mips/usr/include/android/api-level.h \
+ D:/Android/android-ndk-r10d/platforms/android-19/arch-mips/usr/include/malloc.h \
+ D:/Android/android-ndk-r10d/platforms/android-19/arch-mips/usr/include/jni.h
+
+D:/Android/android-ndk-r10d/platforms/android-19/arch-mips/usr/include/string.h:
+
+D:/Android/android-ndk-r10d/platforms/android-19/arch-mips/usr/include/sys/cdefs.h:
+
+D:/Android/android-ndk-r10d/platforms/android-19/arch-mips/usr/include/sys/cdefs_elf.h:
+
+D:/Android/android-ndk-r10d/platforms/android-19/arch-mips/usr/include/android/api-level.h:
+
+D:/Android/android-ndk-r10d/platforms/android-19/arch-mips/usr/include/malloc.h:
+
+D:/Android/android-ndk-r10d/platforms/android-19/arch-mips/usr/include/jni.h:
---
+++ HelloJni/obj/local/mips64/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,22 @@
+obj/local/mips64/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/string.h \
+ D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/sys/cdefs.h \
+ D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/sys/cdefs_elf.h \
+ D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/android/api-level.h \
+ D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/malloc.h \
+ D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/xlocale.h \
+ D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/jni.h
+
+D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/string.h:
+
+D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/sys/cdefs.h:
+
+D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/sys/cdefs_elf.h:
+
+D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/android/api-level.h:
+
+D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/malloc.h:
+
+D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/xlocale.h:
+
+D:/Android/android-ndk-r10d/platforms/android-21/arch-mips64/usr/include/jni.h:
